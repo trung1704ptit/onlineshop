24389f59599d155aa8d305a076156ec35a3839ad
Trung Nguyen <trung1704ptit@gmail.com>
2022-03-24 17:23:42 +0700

    add sub category

--- components/Category/CategoryList.js
+++ components/Category/CategoryList.js
@@ -1,6 +1,4 @@
-import { ListGroup } from "react-bootstrap";
 import Link from "next/link";
-import { BiFootball } from "react-icons/bi";
 import { IoChevronForwardOutline } from "react-icons/io5";
 import isEmpty from "lodash/isEmpty";
 import styles from "../../styles/category.module.scss";
@@ -12,7 +10,7 @@ export default function CategoryList() {
       <ul className="list-style-none">
         {mockList &&
           mockList.map((item) => (
-            <li key={item.id}>
+            <li key={item.id} className={styles["category-item"]}>
               <Link href="#">
                 <a>
                   <item.icon size="25" className={styles["icon"]} />
@@ -27,6 +25,128 @@ export default function CategoryList() {
             </li>
           ))}
       </ul>
+      <div className={styles["sub-category-list"]}>
+        <div className={styles["sub-category-block"]}>
+          <div>
+            <h3 className={styles["sub-cat-title"]}>smartphones</h3>
+            <ul className="list-style-none">
+              <li>
+                <Link href="#">
+                  <a className="link-animated">iPhone 14 Pro</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Samsung Galaxy</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Realme Note</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Sony Xperia Series</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles["category-img"]}>
+            <img src="/images/iphone13pro.png" alt="category-img" />
+          </div>
+        </div>
+        <div className={styles["sub-category-block"]}>
+          <div>
+            <h3 className={styles["sub-cat-title"]}>laptop</h3>
+            <ul className="list-style-none">
+              <li>
+                <Link href="#">
+                  <a className="link-animated">iPhone 14 Pro</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Samsung Galaxy</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Realme Note</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Sony Xperia Series</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles["category-img"]}>
+            <img src="/images/macbook.jpg" alt="category-img" />
+          </div>
+        </div>
+        <div className={styles["sub-category-block"]}>
+          <div>
+            <h3 className={styles["sub-cat-title"]}>headphone</h3>
+            <ul className="list-style-none">
+              <li>
+                <Link href="#">
+                  <a className="link-animated">iPhone 14 Pro</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Samsung Galaxy</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Realme Note</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Sony Xperia Series</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles["category-img"]}>
+            <img src="/images/headphone.png" alt="category-img" />
+          </div>
+        </div>
+        <div className={styles["sub-category-block"]}>
+          <div>
+            <h3 className={styles["sub-cat-title"]}>Xbox</h3>
+            <ul className="list-style-none">
+              <li>
+                <Link href="#">
+                  <a className="link-animated">iPhone 14 Pro</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Samsung Galaxy</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Realme Note</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a className="link-animated">Sony Xperia Series</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles["category-img"]}>
+            <img src="/images/xbox.jpg" alt="category-img" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
