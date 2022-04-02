88e08f8891ff230ce8db6f6abc82334e83b62cbe
Trung Nguyen <trung1704ptit@gmail.com>
2022-04-02 11:05:36 +0700

    add newsletter

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,6 +2,7 @@ import Header from'../Header';
 import Footer from '../Footer';
 import ScrollToTop from 'react-scroll-to-top';
 import { FaArrowUp } from 'react-icons/fa'
+import Newsletter from '../../containers/Newsletters';
 
 
 export default function Layout({ children }) {
@@ -9,8 +10,9 @@ export default function Layout({ children }) {
     <div id="wrap">
       <Header />
       <main>{children}</main>
+      <Newsletter />
       <Footer />
-      <ScrollToTop smooth color="#6f00ff" component={<FaArrowUp />}/>
+      <ScrollToTop smooth color="#6f00ff" component={<FaArrowUp />} />
     </div>
   );
 }
---
+++ containers/Newsletters/index.js
@@ -0,0 +1,35 @@
+import { Col, Container, Row } from "react-bootstrap";
+import styles from "../../styles/newsletter.module.scss";
+
+export default function Newsletter() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section>
+      <div
+        className={styles["newsletter"]}
+        style={{ backgroundImage: "url(/images/pattern-2.png)" }}
+      >
+        <Container>
+          <Row>
+            <Col xs={12} md={6}>
+              <h2>Sign Up For Newsletter</h2>
+              <p>
+                Get E-mail updates about our latest shop and{" "}
+                <span className={styles["highlight"]}>special offers</span>.
+              </p>
+            </Col>
+            <Col xs={12} md={6}>
+              <form className={styles["search-form"]} onSubmit={onSubmit}>
+                <input placeholder="Your email address" required />
+                <button>Sign Up</button>
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </section>
+  );
+}
