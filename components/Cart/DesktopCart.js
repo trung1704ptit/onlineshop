7f045ac7f8108e0cdb54a3f099c51eadda1408a8
Trung Nguyen <trung1704ptit@gmail.com>
2022-04-15 14:21:37 +0700

    add dropdown by antd

--- components/Cart/DesktopCart.js
+++ components/Cart/DesktopCart.js
@@ -4,58 +4,54 @@ import cx from "classnames";
 import dynamic from "next/dynamic";
 
 import styles from "../../styles/header.module.scss";
-import cartStyles from '@styles/cart.module.scss';
+import cartStyles from "@styles/cart.module.scss";
 import { useSelector } from "react-redux";
+import { Card, Dropdown } from "antd";
 
 const EmptyCart = dynamic(() => import("./EmptyCart"));
 const CartPreview = dynamic(() => import("./CartPreview"));
 
 export default function DesktopCart() {
-  const [active, setActive] = useState(false);
   const products = useSelector((state) => state.cart.products);
   const total = useSelector((state) => state.cart.total);
   const totalPrice = useSelector((state) => state.cart.totalPrice);
 
+  const DropdownContent = () => (
+    <Card style={{ width: 400 }} className="p-4">
+      <div className={cartStyles["dropdown-menu-content"]}>
+        {total === 0 ? (
+          <EmptyCart />
+        ) : (
+          <CartPreview products={products} totalPrice={totalPrice} />
+        )}
+      </div>
+      <div className={cartStyles["cart-preview-bottom"]}>
+        Free shipping for All Orders
+      </div>
+    </Card>
+  );
 
   return (
-    <div
-      className="col-auto head-col d-flex"
-      onMouseEnter={() => setActive(true)}
-      onMouseLeave={() => setActive(false)}
-    >
-      {/* cart */}
+    <div className="col-auto head-col d-flex">
       <div className={styles["header-module"]}>
-        <div className={cartStyles["cart-dropdown"]}>
-          <span className={styles["circle-item"]}>
-            <BsCart2 />
-            <span className={cartStyles["total-item"]} data-cy='desktop-cart-total'>{total}</span>
-          </span>
+        <Dropdown overlay={DropdownContent} placement="bottomRight">
+          <div className={cartStyles["cart-dropdown"]}>
+            <span className={styles["circle-item"]}>
+              <BsCart2 />
+              <span
+                className={cartStyles["total-item"]}
+                data-cy="desktop-cart-total"
+              >
+                {total}
+              </span>
+            </span>
 
-          <div className={cartStyles["cart-total-cost"]}>
-            <span className={cartStyles["cart-total-text"]}>Total</span>
-            <span>${totalPrice}</span>
-          </div>
-
-          <div
-            className={cx(
-              "module-dropdown",
-              "dropdown-fade-onhover",
-              "right",
-              active ? "active" : ""
-            )}
-          >
-            <div className={cartStyles["dropdown-menu-content"]}>
-              {total === 0 ? (
-                <EmptyCart />
-              ) : (
-                <CartPreview products={products} totalPrice={totalPrice} />
-              )}
-            </div>
-            <div className={cartStyles["cart-preview-bottom"]}>
-              Free shipping for All Orders
+            <div className={cartStyles["cart-total-cost"]}>
+              <span className={cartStyles["cart-total-text"]}>Total</span>
+              <span>${totalPrice}</span>
             </div>
           </div>
-        </div>
+        </Dropdown>
       </div>
     </div>
   );
--- components/Header/MyAccount.js
+++ components/Header/MyAccount.js
@@ -1,45 +1,33 @@
-import { useState } from "react";
 import Link from "next/link";
-import cx from "classnames";
 import { RiArrowDownSLine } from "react-icons/ri";
-import styles from "../../styles/header.module.scss";
+import { Dropdown, Menu } from "antd";
+import cx from "classnames";
+import styles from "@styles/header.module.scss";
 
 export default function MyAccount() {
-  const [active, setActive] = useState(false);
-
   return (
     <div className={styles["header-module"]}>
-      <div
-        className={cx(styles["my-account"], "pos-rel")}
-        onMouseEnter={() => setActive(true)}
-        onMouseLeave={() => setActive(false)}
-      >
-        <span>My Account</span>
-        <RiArrowDownSLine className={styles["downdown-icon"]} />
-        <div
-          className={cx(
-            "module-dropdown",
-            "dropdown-fade-onhover",
-            "left",
-            active ? "active" : ""
-          )}
-        >
-          <div className={styles["dropdown-menu-content"]}>
-            <ul className="list-style-none">
-              <li>
-                <Link href="/my-account" target="_blank">
-                  <a className="link-animated">Signup</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/my-account" target="_blank">
-                  <a className="link-animated">Login</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
+      <Dropdown overlay={AccountMenu}>
+        <div className={cx(styles["my-account"], "pos-rel")}>
+          <span>My Account</span>
+          <RiArrowDownSLine className={styles["downdown-icon"]} />
         </div>
-      </div>
+      </Dropdown>
     </div>
   );
 }
+
+const AccountMenu = () => (
+  <Menu style={{ width: 180 }}>
+    <Menu.Item>
+      <Link href="/login">
+        <a rel="noopener noreferrer">Login</a>
+      </Link>
+    </Menu.Item>
+    <Menu.Item>
+      <Link href="/signup">
+        <a rel="noopener noreferrer">Sign Up</a>
+      </Link>
+    </Menu.Item>
+  </Menu>
+);
