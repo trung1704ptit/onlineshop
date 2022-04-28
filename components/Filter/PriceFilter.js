4f87343f68043e5257dc146ce73b3fb0797a9d64
Trung Nguyen <trung1704ptit@gmail.com>
2022-04-28 10:35:55 +0700

    show filter price default value

--- components/Filter/PriceFilter.js
+++ components/Filter/PriceFilter.js
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "@styles/filter.module.scss";
 import { Divider, Slider } from "antd";
 import cx from "classnames";
 import { useRouter } from "next/router";
 
+const initialPrice = {
+  min: 0,
+  max: 5000,
+};
+
 export default function PriceFilter() {
-  const [state, setState] = useState([0, 5000]);
+  const [state, setState] = useState([initialPrice.min, initialPrice.max]);
+
   const router = useRouter();
 
-  const onAfterChange = (value) => {
+  const onChange = (value) => {
     setState(value);
+  };
+
+  const onAfterChange = (value) => {
     const query = router.query;
     const pathname = router.pathname;
 
@@ -23,18 +32,30 @@ export default function PriceFilter() {
     router.push(url);
   };
 
+  useEffect(() => {
+    const { query } = router;
+    if (query && (query.minPrice || query.maxPrice)) {
+      const { minPrice, maxPrice } = query;
+      setState([
+        parseFloat(minPrice) || initialPrice.min,
+        parseFloat(maxPrice) || initialPrice.max,
+      ]);
+    }
+  }, [router]);
+
   return (
     <div className={cx(styles["price-slider"], styles["filter-item"])}>
       <h6 className={styles["title"]}>Filter by price</h6>
 
       <Slider
         range
-        defaultValue={[state[0], state[1]]}
+        value={[state[0], state[1]]}
         step={1}
         className="m-0"
         min={0}
         max={10000}
         onAfterChange={onAfterChange}
+        onChange={onChange}
       />
 
       <div className={styles["text-price"]}>
